d20111651099023d9ba3c88ef9c7ce33a298b4db
Golemwulf <adams.timothy.w@gmail.com>
2025-01-14 11:33:52 -0600

    first commit

---
+++ src/main/java/record/label/RecordLabelApplication.java
@@ -0,0 +1,13 @@
+package record.label;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordLabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordLabelApplication.class, args);
+	}
+
+}
---
+++ src/main/java/record/label/controller/RecordLabelController.java
@@ -0,0 +1,13 @@
+package record.label.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/record_label")
+@Slf4j
+public class RecordLabelController {
+
+}
